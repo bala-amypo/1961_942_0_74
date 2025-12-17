589d6b89622ba938df9368fc2c04be7fa0ae9d5b
bala-amypo <bala@amypo.in>
2025-12-17 09:41:04 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -3,10 +3,14 @@ package com.example.demo.Serviceimpl;
 import org.springframe.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 @Service 
 public class StudentServiceImple implements StudentService{
-    private final StudentRepository 
+    private final StudentRepository studentRepository;
+    public StudentServiceImple(StudentRepository studentRepository){
+        this.studentRepository=studentRepository;
+    }
 
     public Student saveStudent(Student student){
         return
