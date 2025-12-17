5ef682ea9a26638c944049c80ecf2028c13e9004
bala-amypo <bala@amypo.in>
2025-12-17 09:21:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -0,0 +1,8 @@
+package com.example.demo.Serviceimpl;
+
+import org.springframe.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Student;
+@Service 
+public class StudentServiceImple
+
