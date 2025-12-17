139596b51f9b3d375a4e9f642b0f1ee6a97a38ef
bala-amypo <bala@amypo.in>
2025-12-17 09:26:02 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -4,5 +4,8 @@ import org.springframe.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Student;
 @Service 
-public class StudentServiceImple
+public class StudentServiceImple implements StudentService{
+
+    
+}
 
